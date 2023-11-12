c0aaa681aafbd88318b1bc000967797dbd602300
Dmytro Ivanishin <dmytroivanishin@gmail.com>
2023-11-12 13:51:39 +0300

    export types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { Tokenizer } from "./tokenizer";
 export { Iterator } from "./iterator";
 export { ASTNode, Parser } from "./parser";
 export { SimpleASTCalculator } from "./compiler";
+
+export * from './types';
